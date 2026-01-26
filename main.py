f2b8e0285c0e759cb3c7dd737957d5971cec8a82
JCHSH <jiangchenshuo@Gmail.com>
2026-01-27 06:48:58 +0800

    2.0正式版，直接穿透zrok，流式请求返回，跟本地一样的效果

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import uvicorn
 if __name__ == "__main__":
     import app
     port = int(os.getenv("PORT", "8080"))
-    uvicorn.run(app.app, host="0.0.0.0", port=port, proxy_headers=True, forwarded_allow_ips="*")
+    uvicorn.run(
+        app.app,
+        host="0.0.0.0",
+        port=port,
+        proxy_headers=True,
+        forwarded_allow_ips="*",
+        ws_ping_interval=30, 
+        ws_ping_timeout=60,
+        log_level="info",     # 可选
+    )
